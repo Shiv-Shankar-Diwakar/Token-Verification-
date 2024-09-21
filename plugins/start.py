8fc87415abba85ff22a89b2063448c2e31c341f9
Shiv-Shankar-Diwakar <shivshankardiwakar702@gmail.com>
2024-09-21 08:28:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -108,7 +108,7 @@ async def start_command(client: Client, message: Message):
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-        return
+
     elif verify_status['is_verified']:
         reply_markup = InlineKeyboardMarkup(
             [
